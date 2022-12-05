e18d12487003ae2107568763fbe0179b7778ac4b
Максим Маркелов <M.Markelov@iepi.ru>
2022-12-05 14:23:31 +0300

    Initial commit

---
+++ cppl-hw-3.1/cppl-hw-3.1.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class SmartArrayCreateException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "SmartArray не может быть создан";
+	}
+};
+
+class SmartArrayBadIndexException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "Недопустимый индекс элемента";
+	}
+};
+
+class SmartArrayEmptySpaceException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "Недостаточный размер массива";
+	}
+};
+
+class smart_array {
+private:
+	int* array = nullptr;
+	int size = 0;
+	int newIndex = 0;
+
+public:
+	smart_array(int size) {
+		if (size > 0) {
+			try {
+				array = new int[size]();
+				this -> size = size;
+			}
+			catch (const std::exception& ex) {
+				throw SmartArrayCreateException();
+			}			
+		}
+		else {
+			throw SmartArrayCreateException();
+		}
+
+	}
+
+	~smart_array() {
+		delete[] array;
+	}
+
+	int add_element(int element) {
+		if (newIndex < size) {
+			array[newIndex] = element;
+			newIndex++;
+			return newIndex - 1;
+		}
+		else {
+			throw SmartArrayEmptySpaceException();
+			//return -1;
+		}		
+	}
+
+	int get_element(int index) {
+		if ((index >= 0) && (index < size)) {
+			return array[index];
+		}
+		else {
+			throw SmartArrayBadIndexException();
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(7) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
---
+++ cppl-hw-3.2/cppl-hw-3.2.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class SmartArrayCreateException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "SmartArray не может быть создан";
+	}
+};
+
+class SmartArrayBadIndexException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "Недопустимый индекс элемента";
+	}
+};
+
+class SmartArrayEmptySpaceException : public std::exception
+{
+public:
+	const char* what() const override {
+		return "Недостаточный размер массива";
+	}
+};
+
+class smart_array {
+private:
+	int* array = nullptr;
+	int size = 0;
+	int newIndex = 0;
+
+public:
+	smart_array(int size) {
+		if (size > 0) {
+			try {
+				array = new int[size]();
+				this->size = size;
+			}
+			catch (const std::exception& ex) {
+				throw SmartArrayCreateException();
+			}
+		}
+		else {
+			throw SmartArrayCreateException();
+		}
+
+	}
+
+	~smart_array() {
+		delete[] array;
+	}
+
+	int add_element(int element) {
+		if (newIndex < size) {
+			array[newIndex] = element;
+			newIndex++;
+			return newIndex - 1;
+		}
+		else {
+			throw SmartArrayEmptySpaceException();
+		}
+	}
+
+	int get_element(int index) {
+		if ((index >= 0) && (index < size)) {
+			return array[index];
+		}
+		else {
+			throw SmartArrayBadIndexException();
+		}
+	}
+
+	int getSize() {
+		return size;
+	}
+
+	int getNewIndex() {
+		return newIndex;
+	}
+	
+	smart_array& operator=(smart_array& other) {
+		for (int i = 0; i < newIndex + 1; i++) {
+			if (i < other.getNewIndex()) {
+				array[i] = other.get_element(i);
+			}
+			else {
+				array[i] = 0;
+			}
+		}
+		newIndex = other.getNewIndex();
+		return *this;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+    smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+	std::cout << "First array\n";
+	for (int i = 0; i < arr.getNewIndex(); i++) {
+		std::cout << arr.get_element(i) << "\t";
+	}
+	std::cout << "\n";
+
+	std::cout << "Second array\n";
+	for (int i = 0; i < new_array.getNewIndex(); i++) {
+		std::cout << new_array.get_element(i) << "\t";
+	}
+	std::cout << "\n";
+
+
+	arr = new_array;
+
+	std::cout << "Result of =\n";
+	for (int i = 0; i < arr.getNewIndex(); i++) {
+		std::cout << arr.get_element(i) << "\t";
+	}
+	std::cout << "\n";
+}
